1e5cd40ca59d848a602cf6c1b4521781cd2e2d5a
luka onikadze <lukaonik@gmail.com>
2026-04-21 14:47:41 +0400

    remove website

--- website/src/routes/(prestige)/learn.lazy.tsx
+++
@@ -1,5 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
-import sidebar from "virtual:prestige/sidebar/learn";
-import { CollectionRoute } from "@lonik/prestige/ui";
-
-export const Route = createLazyFileRoute('/(prestige)/learn')(CollectionRoute(sidebar, "learn"));
--- website/src/routes/(prestige)/learn.showcase.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import * as contentData from "virtual:prestige/content/learn/showcase";
-import { ContentRoute } from "@lonik/prestige/ui";
-
-export const Route = createFileRoute('/(prestige)/learn/showcase')(ContentRoute(contentData));
